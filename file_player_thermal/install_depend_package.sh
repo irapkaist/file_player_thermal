8074a17afadc0a73978c258651c600da6e161070
Intelligent Robotic Autonomy and Perception Lab, KAIST <irapkaist@gmail.com>
2021-09-01 12:41:22 +0900

    Delete file_player_thermal directory

--- file_player_thermal/install_depend_package.sh
+++
@@ -1,9 +0,0 @@
-cd ..
-git clone https://github.com/irapkaist/irp_sen_msgs.git
-git clone https://github.com/swri-robotics/novatel_gps_driver.git
-
-cd novatel_gps_driver
-mv novatel_gps_msgs ../
-
-cd ..
-rm -rf novatel_gps_driver
