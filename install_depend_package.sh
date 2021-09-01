d29f4226f41500e274f231819054e17f47a6a645
Intelligent Robotic Autonomy and Perception Lab, KAIST <irapkaist@gmail.com>
2021-09-01 12:35:18 +0900

    Add files via upload

--- install_depend_package.sh
+++ install_depend_package.sh
@@ -3,6 +3,6 @@ git clone https://github.com/irapkaist/irp_sen_msgs.git
 git clone https://github.com/swri-robotics/novatel_gps_driver.git
 
 cd novatel_gps_driver
-#mv novatel_gps_msgs ../
+mv novatel_gps_msgs ../
 
-#rm -rf novatel_gps_driver
+rm -rf novatel_gps_driver
